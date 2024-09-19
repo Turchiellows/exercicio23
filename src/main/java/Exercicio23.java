b030704731bc96c00bb6e03bfd92d0386fee57f2
root <root@DELL-7559>
2024-09-18 22:05:43 -0300

    Início do projeto

---
+++ src/main/java/Exercicio23.java
@@ -0,0 +1,31 @@
+// IMPORTS
+
+import java.util.Scanner;
+
+public class Exercicio23 {
+
+	public static void main(String[] args) {
+		// INSTANTIATE
+		Scanner sc = new Scanner(System.in);
+
+		// DECLARATE
+		int s = 0;
+		int e = 0;
+		int cnt = 0;
+		int result = 0;
+
+		// COLLECT DATA
+		System.out.print("START : ");
+		s = sc.nextInt();
+		System.out.print("END : ");
+		e = sc.nextInt();
+
+		// OUTPUT
+		cnt = s;
+		while (cnt <= e) {
+			result += cnt;
+			cnt++;
+		}
+		System.out.println("SUM: " + result);
+	}
+}
